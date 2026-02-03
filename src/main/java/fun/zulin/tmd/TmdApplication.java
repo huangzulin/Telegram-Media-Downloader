039837bc097d34a518d9ba8f9437c2200dacb885
zulin <zu-lin@qq.com>
2026-02-03 23:53:30 +0800

    action

--- src/main/java/fun/zulin/tmd/TmdApplication.java
+++ src/main/java/fun/zulin/tmd/TmdApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TmdApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TmdApplication.class, args);
     }
